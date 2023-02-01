1f2615ee7b939eab195df1023b785d575cc3906d
unknown <IsmailBouazza>
2023-02-01 21:54:14 +0100

    npm install

--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-//import { ethers } from "ethers";
+import { ethers } from "ethers";
 import {
   HomeModernIcon,
   KeyIcon,
