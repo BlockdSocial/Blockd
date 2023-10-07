9de81016fa6481207397499634a75ebc6b88b89a
ismailBouazza <ismailbzz1998@gmail.com>
2023-10-07 19:17:56 +0100

    Live chart Updates

--- components/Widgets/Result.tsx
+++ components/Widgets/Result.tsx
@@ -38,7 +38,7 @@ function Result({ result, setInput, searchInput, setSearchInput }: any) {
       <div
         onClick={() => setSearchInput(searchInput)}
         key={result?.id}
-        className="flex items-center justify-start space-x-2 hover:rounded-t-md hover:bg-gray-200 dark:hover:bg-lightgray p-2 w-full cursor-pointer"
+        className="flex items-center justify-start space-x-2 hover:bg-gray-200 dark:hover:bg-lightgray p-2 w-full cursor-pointer"
       >
         <Image
           src={!isEmpty(result?.profilePic)
